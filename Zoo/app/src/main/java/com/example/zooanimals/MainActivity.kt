86f5b0eb7dd53a18916d11ac229c9532379bbd1c
My AnFeVaGa <Job.andresvalencia@gmail.com>
2021-11-29 22:25:46 -0500

    App zoo in progress,apply the functionality of the objects.

--- Zoo/app/src/main/java/com/example/zooanimals/MainActivity.kt
+++ Zoo/app/src/main/java/com/example/zooanimals/MainActivity.kt
@@ -2,13 +2,59 @@ package com.example.zooanimals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.zooanimals.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    val animalObject = AnimalObject()
+    val instanceOfZoologico = Zoologico()
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.saveButtonDate.setOnClickListener { saveAnimalData() }
+        binding.consultButton.setOnClickListener { consultAnimal() }
+    }
+
+    fun saveAnimalData() {
+        val name = binding.enterNameAnimal.text.toString()
+        val specie = binding.enterSpecieAnimal.text.toString()
+        val food = binding.enterFoodAnimal.text.toString()
+
+        val animalObject = AnimalObject(name, specie, food)
+        instanceOfZoologico.listAnimalsZoo.add(animalObject)
 
     }
+
+    fun consultAnimal() {
+        val allAnimal = binding.optionsConsult.checkedRadioButtonId
+        val consult = when (allAnimal) {
+            R.id.all_herbivores -> ""
+            R.id.all_carnivores -> ""
+            else -> ""
+        }
+
+        binding.printResult.text = consult
+    }
+
+
+
+
+
+
+
+
 }
+
+
+// instanceOfZoologico.getCarnivoresAnimals()
+//
+//
+//
+//       instanceOfZoologico.getHerbivoresAnimals()
+//       instanceOfZoologico.getCarnivoresAnimals()
+//       instanceOfZoologico.getAllAnimals()
