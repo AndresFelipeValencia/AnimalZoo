f8c47ab7e6b67cff12e6bdb37149384dc32110c1
My AnFeVaGa <Job.andresvalencia@gmail.com>
2021-12-13 23:49:14 -0500

    Application in progress

---
+++ Zoo/app/src/main/java/com/example/zooanimals/EnumSubject.kt
@@ -0,0 +1,5 @@
+package com.example.zooanimals
+
+enum class EnumSubject {
+    MATHEMATICS, SCIENCES, SPANISH
+}
---
+++ Zoo/app/src/main/java/com/example/zooanimals/StudentObject.kt
@@ -0,0 +1,38 @@
+package com.example.zooanimals
+
+import android.util.Log
+
+class StudentObject(
+    val nameStudent: String, val gradeStudent: String, val listSubjects:
+    MutableList<EnumSubject> = mutableListOf()) {
+
+
+    fun isMathematic(): Boolean {
+        return if (listSubjects.contains(EnumSubject.MATHEMATICS)) {
+            true
+        } else listSubjects.contains(EnumSubject.MATHEMATICS)
+    }
+
+    fun isScience(): Boolean {
+        return if (listSubjects.contains(EnumSubject.SCIENCES)) {
+            true
+        } else listSubjects.contains(EnumSubject.SCIENCES)
+    }
+
+    fun isSpanish(): Boolean {
+        return if (listSubjects.contains(EnumSubject.SPANISH)) {
+            true
+        } else listSubjects.contains(EnumSubject.SPANISH)
+    }
+
+    fun printStudent(i: Int) {
+        Log.d(
+            "StudentObject", "Student's name: " + nameStudent + " " + "Its grade is: "
+                + gradeStudent
+        )
+        for (subject in listSubjects) {
+            Log.d("StudentObject", "Your registered subjects are: $listSubjects")
+        }
+    }
+
+}
